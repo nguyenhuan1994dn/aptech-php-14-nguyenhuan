6581f03d1d407ebc3072d631ed3b7e28494fc6ff
nguyenhuan1994dn <nguyenhuan1994dn@gmail.com>
2018-08-18 16:00:46 +0700

    bat tat den

---
+++ 2018-08-18/javascript.js
@@ -0,0 +1,21 @@
+function turnOn() {
+    var img = document.getElementById("js-bulb");
+    img.src = "pic_bulbon.gif";
+}
+
+function turnOff() {
+    var img = document.getElementById("js-bulb");
+    img.src = "pic_bulboff.gif";
+}
+
+/*
+
+function turnOn() {
+    var img = document.getElementById('js-bulb');
+    img.src = './pic_bulbon.gif';
+}
+
+function turnOff() {
+    var img = document.getElementById('js-bulb');
+    img.src = './pic_bulboff.gif';
+}
