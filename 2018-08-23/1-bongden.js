02b993d54b0d0e9a1d01e99e924321c458f28fdf
nguyenhuan1994dn <nguyenhuan1994dn@gmai.com>
2018-08-24 20:53:43 +0700

    carousel

--- 2018-08-23/1-bongden.js
+++ 2018-08-23/1-bongden.js
@@ -1,11 +1,26 @@
-$(document).ready(function () {
-    $("#congtac").click(function () {
-        $("#bongden").attr("src", function (i, val) {
-            if (val.includes("off")) {
-                return "./pic_bulbon.gif"
-            } else {
-                return "./pic_bulboff.gif"
-            }
-        })
-    })
-})
+/*$(document).ready(function() {
+  $("#congtac").click(function() {
+    $("#bongden").attr("src", function(i, val) {
+      if (val.includes("off")) {
+        return "./pic_bulbon.gif";
+      } else {
+        return "./pic_bulboff.gif";
+      }
+    });
+  });
+});
+*/
+$(document).ready(function() {
+  $("#congtac").click(function() {
+    $("#bongden").attr("src", function(i, val) {
+      console.log(i);
+      console.log(val);
+      if (val.includes("on")) {
+        return "./pic_bulboff.gif";
+      } else {
+        return "./pic_bulbon.gif";
+      }
+    });
+  });
+});
+
